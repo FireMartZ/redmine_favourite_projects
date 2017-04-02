b87d0d099a2896a3b7d9a96ca7dabbf46f12c8c7
FireMartZ <martindube@martindube.com>
2017-04-02 02:32:29 -0400

    Close the select box when closing the responsive flyout

--- assets/javascripts/redmine_improved_searchbox.js
+++ assets/javascripts/redmine_improved_searchbox.js
@@ -5,4 +5,16 @@ $(function(){
 	$('#project_quick_jump_box').select2({
 		placeholder: str
 	});
+
+    // Close the select box when closing the responsive flyout
+    $('.js-flyout-menu-toggle-button').on('click', function(e) {
+        if($('html').hasClass('flyout-is-active')) {
+            $('#wrapper2').on('click', function(e){
+                if(!$('html').hasClass('flyout-is-active')) {
+                    $('#project_quick_jump_box').select2("close");
+                }
+            })
+        }
+    });
 });
+
